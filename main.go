6fab9e05fdcef818031315dbd5f798e21ba6adb4
Neeraj Jaiswal <neeraj@appnimi.com>
2020-05-14 18:28:41 +0530

    Update main.go

--- main.go
+++ main.go
@@ -22,7 +22,8 @@ var _ flutter.Plugin = &Example{}
 func (p *Example) InitPlugin(messenger plugin.BinaryMessenger) error {
 	p.channel = plugin.NewMethodChannel(messenger, "com.neer.godart", plugin.StandardMethodCodec{})
 	p.channel.HandleFunc("hello", hello)
-	p.channel.HandleFunc("sendLater", sendLater)
+	// p.channel.HandleFunc("sendLater", sendLater)
+	p.channel.HandleFunc("mutualCall", p.mutualCall)
 	p.channel.HandleFunc("getError", getErrorFunc)
 	// p.channel.CatchAllHandleFunc(catchAllTest)
 
@@ -36,16 +37,31 @@ func hello(arguments interface{}) (reply interface{}, err error) {
 	return goMsg, nil
 }
 
-func (p *Example) sendLater(arguments interface{}) (reply interface{}, err error) {
-	time.Sleep(3 * time.Second)
-	if rep, err := p.channel.InvokeMethodWithReply("InvokeMethodWithReply", "text_from_golang"); err != nil {
-		log.Println("InvokeMethod error:", err)
-	} else {
-		log.Println("rep.(string) != \"" + rep.(string) + "\"")
-	}
+// func (p *Example) sendLater(arguments interface{}) (reply interface{}, err error) {
+// 	time.Sleep(3 * time.Second)
+// 	if rep, err := p.channel.InvokeMethodWithReply("InvokeMethodWithReply", "text_from_golang"); err != nil {
+// 		log.Println("InvokeMethod error:", err)
+// 	} else {
+// 		log.Println("rep.(string) != \"" + rep.(string) + "\"")
+// 	}
+//
+// 	return "byefromgo", nil
+// }
 
-	return "byefromgo", nil
-}
+
+// mutualCall is attached to the plugin struct
+func (p *Example) mutualCall(arguments interface{}) (reply interface{}, err error) {
+	go func() {
+		time.Sleep(10 * time.Second)
+		if rep, err := p.channel.InvokeMethodWithReply("InvokeMethodWithReply", "text_from_golang"); err != nil {
+			log.Println("InvokeMethod error:", err)
+		} else {
+			if rep.(string) != "text_from_dart" {
+				log.Println("InvokeMethod error: rep.(string) != \"text_from_dart\"")
+				os.Exit(1)
+			}
+		}
+	}()
 
 func getErrorFunc(arguments interface{}) (reply interface{}, err error) {
 	return nil, plugin.NewError("customErrorCode", errors.New("Some error"))
